6adaf2ab0e51b69f80902cae17a33b4db1bc3207
Shengyu Wang <shengyuwang95@gmail.com>
2021-03-09 12:02:06 +1100

    One pass using Set()

---
+++ 36. Valid Sudoku/Solution.js
@@ -0,0 +1,35 @@
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function (board) {
+    for (let i = 0; i < 9; i++) {
+        let row = new Set(),
+            col = new Set(),
+            box = new Set();
+
+        for (let j = 0; j < 9; j++) {
+            // Check first row
+            let _row = board[i][j];
+            // Check first column
+            let _col = board[j][i];
+            // Check first box
+            let _box = board[3 * Math.floor(i / 3) + Math.floor(j / 3)][3 * (i % 3) + (j % 3)]
+
+            if (_row != '.') {
+                if (row.has(_row)) return false;
+                row.add(_row);
+            }
+            if (_col != '.') {
+                if (col.has(_col)) return false;
+                col.add(_col);
+            }
+
+            if (_box != '.') {
+                if (box.has(_box)) return false;
+                box.add(_box);
+            }
+        }
+    }
+    return true;
+};
