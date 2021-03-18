b3c3ca6f31edda71e895c13b8e1062a6e79f07f8
Shengyu Wang <shengyuwang95@gmail.com>
2021-03-19 10:24:54 +1100

    DP fibbo solution

---
+++ 70. Climbing Stairs/Solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+    // Early termination
+    if (n <= 2) {
+        return n
+    }
+    // Initialize DP table
+    const dp = new Array(n + 1).fill(1)
+    // Write DP table
+    for (let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2]
+    }
+    return dp[n]
+};
